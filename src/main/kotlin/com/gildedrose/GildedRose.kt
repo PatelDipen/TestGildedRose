5cc60d09fc53099664b0cd261e1863bbc1003122
dipen.d.patel <dipen.d.patel@accenture.com>
2024-06-06 12:32:08 +0300

    -Declarative parameter naming

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,9 +3,11 @@ package com.gildedrose
 class GildedRose(private var items: List<Item>) {
 
     fun updateQuality() {
-        items.map {
-            val change = it.updatedValue()
-            it.apply {
+        items.map { item ->
+            val change = item.updatedValue()
+            item.apply {
+                // TODO .SELL_IN & .QUALITY should work here instead of .first and .second
+                //  but it is dependent on kotlin version so keeping it simple
                 sellIn = change.first
                 quality = change.second
             }
