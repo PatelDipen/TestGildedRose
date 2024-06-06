bdccb4c7a79d8893d128a535ad9c991db2062240
dipen.d.patel <dipen.d.patel@accenture.com>
2024-06-06 12:50:37 +0300

    -Code refactored

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,13 +3,10 @@ package com.gildedrose
 class GildedRose(private var items: List<Item>) {
 
     fun updateQuality() {
-        items.map { item ->
-            val change = item.updatedValue()
-            item.apply {
-                // TODO .SELL_IN & .QUALITY should work here instead of .first and .second
-                //  but it is dependent on kotlin version so keeping it simple
-                sellIn = change.first
-                quality = change.second
+        items.forEach { item ->
+            item.updatedValue().also { (sellIn, quality) ->
+                item.sellIn = sellIn
+                item.quality = quality
             }
         }
     }
